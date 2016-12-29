450ebb4cc3a422978ae45652723bbfbbdf2f86f3
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2016-12-29 19:21:30 +0100

    :wrench: Added student db Mainly year three students

--- src/js/static/Students.js
+++ src/js/static/Students.js
@@ -2,11 +2,37 @@ const DEV1 = [];
 
 const DEV2 = [
   `lotte.bijlsma`,
+  `julie.van.beethoven`,
 ];
 
 const DEV3 = [
-  `kevin.meyvaert`,
   `thibault.maekelber1`,
+  `kevin.meyvaert`,
+  `emiel.vandenbroeck`,
+  `elise.van.oosten`,
+  `anthony.magnus`,
+  `jonas.leupe`,
+  `wannes.monchy`,
+  `benoit.bosschaert`,
+  `daphne.danneels`,
+  `jonas.devacht`,
+  `jordy.wittouck`,
+  `katia.smet`,
+  `alex.bernard`,
+  `suzan.van.dijck`,
+  `bernd.bousard`,
+  `kevin.hendrickx`,
+  `samuel.de.poorter`,
+  `thorr.stevens`,
+  `dieter.cafmeyer`,
+  `emiel.van.betsbrug1`,
+  `kevin.waelkens`,
+  `nicolaas.koelewijn`,
+  `sebastien.henau`,
+  `maxim.de.groote`,
+  `arne.deprez`,
+  `jesse.de.boitselier`,
+  `gert-jan.wille`,
 ];
 
 export default [`---`, ...DEV1, ...DEV2, ...DEV3];
